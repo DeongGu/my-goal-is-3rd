be37d1cf8769c24104ae3971b5e356d19d7f563e
신성우 <ssw4510@gmail.com>
2023-05-06 15:24:12 +0900

    [Front] CRA 초기세팅

---
+++ my-goal-is-3rd-client/prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "avoid", // 화살표함수괄호사용방식
+  bracketSpacing: true, // 객체리터럴에서괄호에공백삽입여부
+  jsxBracketSameLine: false, // JSX의마지막`>`를다음줄로내릴지여부
+  printWidth: 80, //  줄바꿈할폭길이
+  semi: true, // 세미콜론사용여부
+  singleQuote: true, // single 쿼테이션사용여부
+  tabWidth: 2, // 탭너비
+  trailingComma: "all", // 여러줄을사용할때, 후행콤마사용방식
+  endOfLine: "auto", // 파일 끝에 빈줄 추가
+};
